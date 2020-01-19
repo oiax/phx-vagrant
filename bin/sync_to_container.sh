fa4455c253f4598a22895276aa405362b69e753d
Tsutomu Kuroda <t-kuroda@oiax.jp>
2020-01-19 15:11:16 +0900

    Fix sync scripts

---
+++ bin/sync_to_container.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -eu
+
+rsync -au --exclude 'node_modules/' --exclude '_build/' --exclude 'deps/' \
+  --exclude 'priv/static/' --exclude '*.log' --delete /vagrant/ /work/
--- bin/sync_vagrant_and_work_dirs.sh
+++ bin/sync_vagrant_and_work_dirs.sh
@@ -5,6 +5,6 @@ while true; do
   rsync -au --exclude 'node_modules/' --exclude '_build/' --exclude 'deps/' \
     --exclude 'priv/static/' --exclude '*.log' /vagrant/ /work/
   rsync -au --exclude 'node_modules/' --exclude '_build/' --exclude 'deps/' \
-    --exclude 'priv/static/' --exclude '*.log' /work/ /vagrant/
+    --exclude 'priv/static/' --exclude '*.log' --existing /work/ /vagrant/
   sleep 1
 done
